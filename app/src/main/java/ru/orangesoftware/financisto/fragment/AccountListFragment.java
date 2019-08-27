ef1dc3732e4063a17ce1282cb540aae1e62a8338
Andrey Kravchenko <xabbl4@gmail.com>
2019-08-27 14:42:19 +0300

    Move AccountListAdapter to fragment

--- app/src/main/java/ru/orangesoftware/financisto/fragment/AccountListFragment.java
+++ app/src/main/java/ru/orangesoftware/financisto/fragment/AccountListFragment.java
@@ -10,13 +10,17 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
+import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-import androidx.annotation.Nullable;
 import greendroid.widget.QuickActionGrid;
 import greendroid.widget.QuickActionWidget;
 import ru.orangesoftware.financisto.R;
@@ -31,19 +35,24 @@ import ru.orangesoftware.financisto.activity.MyQuickAction;
 import ru.orangesoftware.financisto.activity.PurgeAccountActivity;
 import ru.orangesoftware.financisto.activity.TransactionActivity;
 import ru.orangesoftware.financisto.activity.TransferActivity;
-import ru.orangesoftware.financisto.adapter.AccountListAdapter2;
 import ru.orangesoftware.financisto.blotter.BlotterFilter;
 import ru.orangesoftware.financisto.blotter.TotalCalculationTask;
 import ru.orangesoftware.financisto.bus.GreenRobotBus_;
 import ru.orangesoftware.financisto.bus.SwitchToMenuTabEvent;
+import ru.orangesoftware.financisto.datetime.DateUtils;
 import ru.orangesoftware.financisto.db.DatabaseAdapter;
 import ru.orangesoftware.financisto.dialog.AccountInfoDialog;
 import ru.orangesoftware.financisto.filter.Criteria;
 import ru.orangesoftware.financisto.model.Account;
+import ru.orangesoftware.financisto.model.AccountType;
+import ru.orangesoftware.financisto.model.CardIssuer;
+import ru.orangesoftware.financisto.model.ElectronicPaymentType;
 import ru.orangesoftware.financisto.model.Total;
 import ru.orangesoftware.financisto.utils.IntegrityCheckAutobackup;
 import ru.orangesoftware.financisto.utils.MyPreferences;
+import ru.orangesoftware.financisto.utils.Utils;
 import ru.orangesoftware.financisto.view.NodeInflater;
+import ru.orangesoftware.orb.EntityManager;
 
 import static ru.orangesoftware.financisto.fragment.BlotterFragment.SAVE_FILTER;
 
@@ -300,11 +309,11 @@ public class AccountListFragment extends AbstractListFragment {
     @Override
     protected void updateAdapter() {
         if(adapter==null){
-            adapter = new AccountListAdapter2(context, cursor);
+            adapter = new AccountListAdapter(context, cursor);
             setListAdapter(adapter);
         }else{
-            ((AccountListAdapter2) adapter).changeCursor(cursor);
-            ((AccountListAdapter2) adapter).notifyDataSetChanged();
+            ((AccountListAdapter) adapter).changeCursor(cursor);
+            ((AccountListAdapter) adapter).notifyDataSetChanged();
         }
     }
 
@@ -358,6 +367,116 @@ public class AccountListFragment extends AbstractListFragment {
     private void showTotals() {
         Intent intent = new Intent(context, AccountListTotalsDetailsActivity.class);
         startActivityForResult(intent, -1);
+    }
+
+    public class AccountListAdapter extends ResourceCursorAdapter {
+
+        private final Utils u;
+        private DateFormat df;
+        private boolean isShowAccountLastTransactionDate;
+
+        public AccountListAdapter(Context context, Cursor c) {
+            super(context, R.layout.account_list_item, c);
+            this.u = new Utils(context);
+            this.df = DateUtils.getShortDateFormat(context);
+            this.isShowAccountLastTransactionDate = MyPreferences.isShowAccountLastTransactionDate(context);
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            View view = super.newView(context, cursor, parent);
+            return AccountListItemHolder.create(view);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            Account a = EntityManager.loadFromCursor(cursor, Account.class);
+            AccountListItemHolder v = (AccountListItemHolder) view.getTag();
+
+            v.centerView.setText(a.title);
+
+            AccountType type = AccountType.valueOf(a.type);
+            if (type.isCard && a.cardIssuer != null) {
+                CardIssuer cardIssuer = CardIssuer.valueOf(a.cardIssuer);
+                v.iconView.setImageResource(cardIssuer.iconId);
+            } else if (type.isElectronic && a.cardIssuer != null) {
+                ElectronicPaymentType paymentType = ElectronicPaymentType.valueOf(a.cardIssuer);
+                v.iconView.setImageResource(paymentType.iconId);
+            } else {
+                v.iconView.setImageResource(type.iconId);
+            }
+            if (a.isActive) {
+                v.iconView.getDrawable().mutate().setAlpha(0xFF);
+                v.iconOverView.setVisibility(View.INVISIBLE);
+            } else {
+                v.iconView.getDrawable().mutate().setAlpha(0x77);
+                v.iconOverView.setVisibility(View.VISIBLE);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            if (!Utils.isEmpty(a.issuer)) {
+                sb.append(a.issuer);
+            }
+            if (!Utils.isEmpty(a.number)) {
+                sb.append(" #").append(a.number);
+            }
+            if (sb.length() == 0) {
+                sb.append(context.getString(type.titleId));
+            }
+            v.topView.setText(sb.toString());
+
+            long date = a.creationDate;
+            if (isShowAccountLastTransactionDate && a.lastTransactionDate > 0) {
+                date = a.lastTransactionDate;
+            }
+            v.bottomView.setText(df.format(new Date(date)));
+
+            long amount = a.totalAmount;
+            if (type == AccountType.CREDIT_CARD && a.limitAmount != 0) {
+                long limitAmount = Math.abs(a.limitAmount);
+                long balance = limitAmount + amount;
+                long balancePercentage = 10000 * balance / limitAmount;
+                u.setAmountText(v.rightView, a.currency, amount, false);
+                u.setAmountText(v.rightCenterView, a.currency, balance, false);
+                v.rightView.setVisibility(View.VISIBLE);
+                v.progressBar.setMax(10000);
+                v.progressBar.setProgress((int) balancePercentage);
+                v.progressBar.setVisibility(View.VISIBLE);
+            } else {
+                u.setAmountText(v.rightCenterView, a.currency, amount, false);
+                v.rightView.setVisibility(View.GONE);
+                v.progressBar.setVisibility(View.GONE);
+            }
+        }
+
+    }
+
+    private static class AccountListItemHolder {
+        ImageView iconView;
+        ImageView iconOverView;
+        TextView topView;
+        TextView centerView;
+        TextView bottomView;
+        TextView rightCenterView;
+        TextView rightView;
+        ProgressBar progressBar;
+
+        public static View create(View view) {
+            AccountListItemHolder v = new AccountListItemHolder();
+            v.iconView = view.findViewById(R.id.icon);
+            v.iconOverView = view.findViewById(R.id.active_icon);
+            v.topView = view.findViewById(R.id.top);
+            v.centerView = view.findViewById(R.id.center);
+            v.bottomView = view.findViewById(R.id.bottom);
+            v.rightCenterView = view.findViewById(R.id.right_center);
+            v.rightView = view.findViewById(R.id.right);
+            v.rightView.setVisibility(View.GONE);
+            v.progressBar = view.findViewById(R.id.progress);
+            v.progressBar.setVisibility(View.GONE);
+            view.setTag(v);
+            return view;
+        }
+
     }
 
 }
