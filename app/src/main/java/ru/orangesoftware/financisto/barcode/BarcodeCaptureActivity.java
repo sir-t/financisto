af6c6e04c83e09c18a25138e939ba69307045457
Andrey Kravchenko <xabbl4@gmail.com>
2019-09-12 17:31:29 +0300

    Set FocusMode to macro. Tap in scan qrcode for autofocus. Replace Base64 to android.util for support API 16

--- app/src/main/java/ru/orangesoftware/financisto/barcode/BarcodeCaptureActivity.java
+++ app/src/main/java/ru/orangesoftware/financisto/barcode/BarcodeCaptureActivity.java
@@ -74,7 +74,8 @@ public final class BarcodeCaptureActivity extends AppCompatActivity
         super.onCreate(icicle);
         setContentView(R.layout.barcode_capture);
 
-        mPreview = (CameraSourcePreview) findViewById(R.id.preview);
+        mPreview = findViewById(R.id.preview);
+        mPreview.setOnClickListener(v -> mCameraSource.autoFocus(null));
 
         boolean autoFocus = true;
         boolean useFlash = false;
@@ -169,7 +170,7 @@ public final class BarcodeCaptureActivity extends AppCompatActivity
         // make sure that auto focus is an available option
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
             builder = builder.setFocusMode(
-                    autoFocus ? Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE : null);
+                    autoFocus ? Camera.Parameters.FOCUS_MODE_MACRO : null);
         }
 
         mCameraSource = builder
--- app/src/main/java/ru/orangesoftware/financisto/barcode/RequestReceiptTask.java
+++ app/src/main/java/ru/orangesoftware/financisto/barcode/RequestReceiptTask.java
@@ -2,11 +2,11 @@ package ru.orangesoftware.financisto.barcode;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Base64;
 import android.util.Log;
 
 import org.json.JSONObject;
 
-import java.util.Base64;
 import java.util.Map;
 
 import okhttp3.OkHttpClient;
@@ -44,48 +44,46 @@ public class RequestReceiptTask extends AsyncTask<String, String, String> {
             String url_1 = "https://proverkacheka.nalog.ru:9999/v1/ofds/*/inns/*/fss/" + mQRCode.get("fn") + "/operations/" + mQRCode.get("n") + "/tickets/" + mQRCode.get("i") + "?fiscalSign=" + mQRCode.get("fp") + "&date=" + mQRCode.get("t") + "&sum=" + mQRCode.get("s").replaceAll("\\.", "");
             String url_2 = "https://proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss/" + mQRCode.get("fn") + "/tickets/" + mQRCode.get("i") + "?fiscalSign=" + mQRCode.get("fp") + "&sendToEmail=no";
 
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                String login = MyPreferences.getNalogLogin(context);
-                String pass = MyPreferences.getNalogPassword(context);
+            String login = MyPreferences.getNalogLogin(context);
+            String pass = MyPreferences.getNalogPassword(context);
 
-                OkHttpClient client = new OkHttpClient();
-                Request.Builder builder = new Request.Builder()
-                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString((login+":"+pass).getBytes()))
-                        .header("Device-Id", "")
-                        .header("Device-OS", "");
+            OkHttpClient client = new OkHttpClient();
+            Request.Builder builder = new Request.Builder()
+                    .header("Authorization", "Basic " + Base64.encodeToString((login+":"+pass).getBytes(), Base64.NO_WRAP))
+                    .header("Device-Id", "")
+                    .header("Device-OS", "");
 
-                Request request = builder.url(url_1).build();
-                Response response_check = client.newCall(request).execute();
-                result = Integer.toString(response_check.code());
+            Request request = builder.url(url_1).build();
+            Response response_check = client.newCall(request).execute();
+            result = Integer.toString(response_check.code());
 
-                if (response_check.code() == 204) {
-                    request = builder.url(url_2).build();
-                    Response response_get = client.newCall(request).execute();
-                    result = Integer.toString(response_get.code());
+            if (response_check.code() == 204) {
+                request = builder.url(url_2).build();
+                Response response_get = client.newCall(request).execute();
+                result = Integer.toString(response_get.code());
 
-                    if (response_get.code() == 202) {
-                        response_get.close();
-                        Thread.sleep(500);
-                        response_get = client.newCall(request).execute();
-                    }
-
-                    if (response_get.code() == 200) {
-                        JSONObject jsonObject;
-                        try {
-                            jsonObject = new JSONObject(response_get.body().string());
-                            jsonObject = jsonObject.getJSONObject("document").getJSONObject("receipt");
-                            jsonObject.remove("rawData");
-                            result = jsonObject.toString();
-                        } catch (Exception ex) {
-                            Log.e("CheckRequest", "Unexpected error", ex);
-                            result = response_get.body().string();
-                        }
-                    }
-
+                if (response_get.code() == 202) {
                     response_get.close();
+                    Thread.sleep(500);
+                    response_get = client.newCall(request).execute();
                 }
-                response_check.close();
+
+                if (response_get.code() == 200) {
+                    JSONObject jsonObject;
+                    try {
+                        jsonObject = new JSONObject(response_get.body().string());
+                        jsonObject = jsonObject.getJSONObject("document").getJSONObject("receipt");
+                        jsonObject.remove("rawData");
+                        result = jsonObject.toString();
+                    } catch (Exception ex) {
+                        Log.e("CheckRequest", "Unexpected error", ex);
+                        result = response_get.body().string();
+                    }
+                }
+
+                response_get.close();
             }
+            response_check.close();
             return result;
         } catch (Exception ex) {
             Log.e("CheckRequest", "Unexpected error", ex);
